0e2384b3ac3e2c29193c25ddf96dc8047b40f772
joyce-bloom <joyce@getbloom.io>
2025-12-04 10:02:50 +0800

    added terms of service link in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
             <ul className="flex flex-col gap-4 pt-4">
               <li className="text-[#4D644D]"><Link href={"/"} className="text-base no-underline">Home</Link></li>
               <li className="text-[#4D644D]"><Link href={"/"} className="text-base no-underline">Privacy Policy</Link></li>
-              <li className="text-[#4D644D]"><Link href={"/"} className="text-base no-underline">Terms of Service</Link></li>
+              <li className="text-[#4D644D]"><Link href={"/terms-of-service"} className="text-base no-underline">Terms of Service</Link></li>
               {/* <li className="text-[#4D644D]"><Link href={"/"} className="text-base no-underline">Subscriptions</Link></li> */}
             </ul>
           </nav>
