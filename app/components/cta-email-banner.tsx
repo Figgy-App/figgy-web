4bc42f0ee7debdaf40688574e5b2371888c3208a
joyce-bloom <joyce@getbloom.io>
2025-12-12 08:04:33 +0800

    anchor 'get in touch' button to contact form

--- app/components/cta-email-banner.tsx
+++ app/components/cta-email-banner.tsx
@@ -7,7 +7,7 @@ export default function CtaEmailBanner() {
         <div className="py-20 px-12 lg:py-32">
           <p className="text-[28px] leading-8 text-[#fff] font-semibold text-center md:text-4xl md:leading-10 lg:text-5xl lg:leading-none">Still have questions?</p>
           <p className="max-w-[480px] my-0 mx-auto pt-3 pb-5 text-sm text-[#fff] text-center md:pt-4 md:text-base lg:text-lg">Can’t find the answer to your question? Send us an email and we’ll get back to you as soon as possible!</p>
-          <Link href={"/"} className="block w-36 py-3 px-6 my-0 mx-auto text-sm md:text-base text-[#6AAD5F] text-center bg-[#F3FAE9] border-solid border-[#F3FAE9] rounded-full">
+          <Link href={"#contact-form"} className="block w-36 py-3 px-6 my-0 mx-auto text-sm md:text-base text-[#6AAD5F] text-center bg-[#F3FAE9] border-solid border-[#F3FAE9] rounded-full">
             Get in touch
           </Link>
         </div>
